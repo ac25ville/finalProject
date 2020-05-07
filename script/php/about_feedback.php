965034d50460574f6fe2081727ae0ce7c420a4df
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-07 18:02:19 -0500

    about page php fix

--- script/php/about_feedback.php
+++ script/php/about_feedback.php
@@ -1,6 +1,6 @@
 <?php
     function writeFeedback(){
-        $file = fopen('feedbackdata.txt', 'a') or die("Unable to open file!");
+        $file = fopen('feedbackdata.txt', 'w') or die("Unable to open file!");
         fwrite($file, $_POST['feedback_name'].'\n');
         fwrite($file, $_POST['feedback_sNum'].'\n');
         fwrite($file, $_POST['feedback_response'].'\n');
