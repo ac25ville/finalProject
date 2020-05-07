801e1d9476d00c3f80c8fb7eb603fc556a20b016
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-06 20:53:43 -0500

    about page php fix

--- script/php/about_feedback.php
+++ script/php/about_feedback.php
@@ -1,14 +1,14 @@
 <?php
-    function printFormValue(){
+function printFormValue(){
         echo $_POST['feedback_name'].' '.$_POST['feedback_sNum'].' '.$_POST['feedback_text'];
     };
 
     printFormValue();
     if($_GET['funct']=='printFormValue'){
         printFromValue();
-        echo Hello World!;
+        echo 'Hello World !';
     }
     
-    echo Hello World!;
+    echo 'Hello World!';
 
 ?>
