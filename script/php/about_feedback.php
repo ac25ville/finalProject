c80cbf25495663640eca9b088bf078600ce3639b
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-06 21:13:42 -0500

    about page php fix

--- script/php/about_feedback.php
+++ script/php/about_feedback.php
@@ -3,6 +3,16 @@
         echo $_POST['feedback_name'].' '.$_POST['feedback_sNum'].' '.$_POST['feedback_text'];
     }
 
+    foreach ($_POST as $key => $value) {
+    echo '<p>'.$key.'</p>';
+    foreach($value as $k => $v)
+    {
+    echo '<p>'.$k.'</p>';
+    echo '<p>'.$v.'</p>';
+    echo '<hr />';
+    }
+
+    }
     printFormValue();
 //    if($_GET['funct']=='printFormValue'){
 //        printFromValue();
