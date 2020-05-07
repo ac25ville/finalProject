6d5f79f347bbf72d8ea3ffeaf5183cef82a52b34
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-07 17:43:32 -0500

    about page php fix

--- script/php/about_feedback.php
+++ script/php/about_feedback.php
@@ -1,11 +1,20 @@
 <?php
-    function printFeedback(){
-        echo $_POST['feedback_name'].' '.$_POST['feedback_sNum'].' '.$_POST['feedback_response'];
+    function writeFeedback(){
+        $file = fopen('feedbackdata.txt', 'a');
+        
+        foreach($_POST['feedback'] as $j){
+            fwrite($file, $j.'\n');
+            echo $j.'\n';
+        }
+        fwrite($file, 'endofpost\n');
+        fclose($myfile);
     }
+
+    
 
     if(isset($_GET['funct'])){
         if($_GET['funct']=='printFeedback'){
-            printFeedback();
+            writeFeedback();
     }
 }
 ?>
