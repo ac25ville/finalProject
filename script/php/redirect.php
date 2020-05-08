5355dfd69e94e53d09265684176f5da77875ea41
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-07 23:21:42 -0500

    added redirect.php

--- script/php/redirect.php
+++ script/php/redirect.php
@@ -4,7 +4,7 @@
     $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $location);
-    exit;
+    exit();
     }   
 
 ?>
