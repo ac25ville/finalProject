31798524ef8c76bed4402843cf6f72b75bedd90f
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-08 00:03:03 -0500

    updated about_feedback file

--- script/php/about_feedback.php
+++ script/php/about_feedback.php
@@ -1,22 +1,41 @@
 <?php
-    function writeFeedback(){
-//        $filename = 'feedbackdata.txt';
-//        $file = fopen($filename, 'a+') or die("Unable to open file!".$file);
-//        echo $file;
-//        fwrite($file, $_POST['feedback_name'].'\n');
-//        fwrite($file, $_POST['feedback_sNum'].'\n');
-//        fwrite($file, $_POST['feedback_response'].'\n');
-//        fwrite($file, 'endofpost\n');
-//        fclose($file);
-        echo $_POST['feedback_name']."\n". $_POST['feedback_sNum']."\n".$_POST['feedback_response']."\n";
-        echo "\n".'<a href="http://ec2-3-134-90-38.us-east-2.compute.amazonaws.com/pages/about.html">Back to About Page</a>';
+    require "redirect.php";
+    function printFeedback(){
+        echo $_POST['feedback_name']."<br>". $_POST['feedback_sNum']."<br>".$_POST['feedback_response']."<br>";
     }
-
-    if(isset($_GET['funct'])){
-        if($_GET['funct']=='printFeedback'){
-            writeFeedback();
-//            sleep(3);
-//            header("Location: http://ec2-3-134-90-38.us-east-2.compute.amazonaws.com/pages/about.html");
-    } 
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--
+    Name: Austin Corteville
+    Pawprint: acc9cm
+    Date: 5/7/2020
+    Final Project
+    
+    This is a php response document
+    
+    @refrences:
+    -->
+<meta charset = "UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="../../css/about.css">
+<title>Bike Resource Center - About the Project</title>
+</head>
+<body>
+    <div class="content">
+        <?php
+        if(isset($_GET['funct'])){
+            if($_GET['funct']=='printFeedback'){
+            printFeedback();
+        } 
+        ?>
+    </div>
+</body>
+</html>
