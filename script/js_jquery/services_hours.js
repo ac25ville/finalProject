852aefda69e7dc1a28191e79799c9ea39067f602
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-08 12:16:14 -0500

    started work on ajax request for service hours page

---
+++ script/js_jquery/services_hours.js
@@ -0,0 +1,8 @@
+//write an ajax call for the hours. this is really your last hard part. Just need to do the Login with another php page after that. Just follow the wergz tutorial on that.
+
+$(function(){
+    var day = "";
+     $.get("../php/hours_request.php?hours=yes&day="+day, function(){
+        alert("success"); 
+     });
+});
---
+++ script/php/hours_request.php
@@ -0,0 +1,18 @@
+<?php
+    function getHours(){
+        $j = $_GET['day'];
+        $j = strtolower($j);
+        switch($j){
+            case 'monday': return "12PM-4PM";
+            case 'tuesday': return "10AM-2PM";
+            case 'wednesday': return "12PM-4PM";
+            case 'thursday': return "10AM-2PM";
+            case 'friday': return "12PM-4PM";
+            default: return "No hours for this day";
+        }
+    }
+    
+    if(isset($_GET['hours'])&&$_GET['hours']=='yes'){
+        getHours();
+    }
+?>
