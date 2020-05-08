143f866f74330756c9cde76ff34c6e28b58692e0
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-07 21:04:55 -0500

    about page php fix

--- script/php/about_feedback.php
+++ script/php/about_feedback.php
@@ -1,14 +1,17 @@
 <?php
     function writeFeedback(){
-        $filename = 'feedbackdata.txt';
-        $file = fopen($filename, 'w') or die("Unable to open file!".$file);
-        echo $file;
-        fwrite($file, $_POST['feedback_name'].'\n');
-        fwrite($file, $_POST['feedback_sNum'].'\n');
-        fwrite($file, $_POST['feedback_response'].'\n');
-        fwrite($file, 'endofpost\n');
+//        $filename = 'feedbackdata.txt';
+//        $file = fopen($filename, 'a+') or die("Unable to open file!".$file);
+//        echo $file;
+//        fwrite($file, $_POST['feedback_name'].'\n');
+//        fwrite($file, $_POST['feedback_sNum'].'\n');
+//        fwrite($file, $_POST['feedback_response'].'\n');
+//        fwrite($file, 'endofpost\n');
+//        fclose($file);
         echo $_POST['feedback_name'].'\n'. $_POST['feedback_sNum'].'\n'.$_POST['feedback_response'].'\n';
-        fclose($file);
+        
+        sleep(8);
+        header("http://ec2-3-134-90-38.us-east-2.compute.amazonaws.com/pages/about.html");
     }
 
     if(isset($_GET['funct'])){
