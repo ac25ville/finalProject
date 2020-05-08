4e47242ad6decc3718a070fe6499321e87e07578
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-05-07 21:46:56 -0500

    about page php fix

--- script/php/about_feedback.php
+++ script/php/about_feedback.php
@@ -14,7 +14,8 @@
     if(isset($_GET['funct'])){
         if($_GET['funct']=='printFeedback'){
             writeFeedback();
-            header("Location: #");
+            sleep(3);
+            header("Location: http://ec2-3-134-90-38.us-east-2.compute.amazonaws.com/pages/about.html");
     } 
 }
 ?>
